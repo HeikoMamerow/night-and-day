c6b206828a8b5e6597672dc5096fc0d0dc7d7903
Heiko Mamerow <mail@heikomamerow.de>
2014-10-03 23:57:15 +0200

    enqeue & css

--- night-and-day.php
+++ night-and-day.php
@@ -3,8 +3,26 @@
  * Plugin Name: Night and day
  * Plugin URI: https://github.com/HeikoMamerow/night-and-day
  * Description: Switch distraction free writing mode in light or dark theme.
- * Version: 0.1
+ * Version: 0.0
  * Author: Heiko Mamerow
  * Author URI: http://heikomamerow.de
  * License: GPL2
  */
+
+
+// Add theme support for custom CSS in the TinyMCE visual editor
+function tiny_mce_css( $mce_css ) {
+	if ( ! empty( $mce_css ) )
+		$mce_css .= ',';
+		$mce_css .= plugins_url( 'css/tinymce.css', __FILE__ );
+		return $mce_css;
+}
+add_filter( 'mce_css', 'tiny_mce_css' );
+
+
+// Enqeue style for the fullscreen overlay
+function overlay_css() {
+	wp_enqueue_style( 'night-and-day', plugins_url( 'css/overlay.css', __FILE__ ) );
+}
+
+add_action( 'admin_enqueue_scripts', 'overlay_css' );
